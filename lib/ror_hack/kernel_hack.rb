9f38fcb4a7cd30adce1fda239ed5fc7c8d86f8a3
happyMing <339755551@qq.com>
2015-08-22 21:31:47 +0800

    加入rails导入yml配置文件的方法

--- lib/ror_hack/kernel_hack.rb
+++ lib/ror_hack/kernel_hack.rb
@@ -4,6 +4,25 @@ module RorHack
     def petty_eval(bind, str)
       eval str, bind
     end
+
+    def yml_load_config(name, default_value='66dc9e58b19ecc4ec538ea771b71b372')
+      name += '.yml' unless name.end_with?('.yml')
+      if File.file?(File.join(Rails.root, '/config', name))
+        result = YAML.load_file(File.join(Rails.root, '/config', name))
+        if result.is_a? Array
+          return result
+        else
+          return OpenStruct.new(result)
+        end
+      else
+        if default_value == '66dc9e58b19ecc4ec538ea771b71b372'
+          YAML.load_file(File.join(Rails.root, '/config', name))
+        else
+          return default_value
+        end
+      end
+    end
+
   end
 end
 
